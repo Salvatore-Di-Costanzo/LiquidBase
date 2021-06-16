e484e5d7bd97cce4fedb524ddeec7222aad4c05d
Salvatore-Di-Costanzo <ForestadiconiferE01!>
2021-06-16 10:40:36 +0200

    Initial commit

---
+++ src/main/java/io/db/LiquidBase/LiquidBaseApplication.java
@@ -0,0 +1,13 @@
+package io.db.LiquidBase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquidBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquidBaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/db/LiquidBase/LiquidBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.db.LiquidBase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquidBaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
